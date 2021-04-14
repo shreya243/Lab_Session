6a7553c4d67a8413044d4999c62e673bcb9f06ac
shreya <shreya24priya@gmail.com>
2021-04-14 15:21:54 +0530

    add student

---
+++ src/com/shreya/demo/model/Author.java
@@ -0,0 +1,24 @@
+package com.shreya.demo.model;
+
+public class Author {
+public int id;
+public String name;
+public Author(int id, String name) {
+	super();
+	this.id = id;
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
