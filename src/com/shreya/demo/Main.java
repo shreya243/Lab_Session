542abcac18c1158fddc538bbdfbf8dbd83e557ac
shreya243 <82446728+shreya243@users.noreply.github.com>
2021-04-14 14:11:16 +0530

    Update Main.java

--- src/com/shreya/demo/Main.java
+++ src/com/shreya/demo/Main.java
@@ -3,5 +3,7 @@ package com.shreya.demo;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println( "Hello World! all" );
+		System.out.println( "Doing changes in main" );
+		
 	}
 }
