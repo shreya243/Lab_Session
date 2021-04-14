dda2901acd9b668392835ca404537bf11d733973
shreya <shreya24priya@gmail.com>
2021-04-14 14:00:03 +0530

    adding employee class

---
+++ src/com/shreya/demo/model/Employee.java
@@ -0,0 +1,23 @@
+package com.shreya.demo.model;
+
+public class Employee {
+public int id;
+public String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Employee(int id, String name) {
+	super();
+	this.id = id;
+	this.name = name;
+}
+}
